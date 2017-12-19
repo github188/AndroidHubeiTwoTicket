a6ae89324fbfdefb098ffa8642439629a0254d15
xieshibao <847019290@qq.com>
2017-12-19 14:52:20 +0800

    谢仕宝--添加

--- app/src/main/java/tickets/nari/com/hubeitickets/adapter/CaoZuoPiao_List_Adapter.java
+++ app/src/main/java/tickets/nari/com/hubeitickets/adapter/CaoZuoPiao_List_Adapter.java
@@ -1,6 +1,7 @@
 package tickets.nari.com.hubeitickets.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,9 +21,11 @@ import tickets.nari.com.hubeitickets.bean.ArchivedTicketListBean;
 public class CaoZuoPiao_List_Adapter extends BaseAdapter {
     private Context mContext;
     private List<ArchivedTicketListBean> ticketList; //   票列表集合
-    public CaoZuoPiao_List_Adapter(Context mContext, List<ArchivedTicketListBean> ticketList) {
+    private String status = "0";
+    public CaoZuoPiao_List_Adapter(Context mContext, List<ArchivedTicketListBean> ticketList,String status) {
         this.mContext = mContext;
         this.ticketList = ticketList;
+        this.status = status;
     }
     @Override
     public int getCount() {
@@ -58,15 +61,19 @@ public class CaoZuoPiao_List_Adapter extends BaseAdapter {
         }
 
 //        String status = ticketList.get(position).getPZT();
-//        if("21".equals(status)) { //   待审核
-////            holder.iv_ygd_status.setImageResource(R.mipmap.daishenhe);
-//        }else if ("31".equals(status)) { //   待执行
-////            holder.iv_ygd_status.setImageResource(R.mipmap.daizhixing);
-//        }else if ("41".equals(status)) { //   执行中
-////            holder.iv_ygd_status.setImageResource(R.mipmap.zhixing);
-//        }else if ("51".equals(status)) { //   已归档
-//
-//        }
+        if("0".equals(status)) { //   待审核
+            holder.iv_ygd_status.setText("待处理");
+
+            holder.iv_ygd_status.setBackgroundResource(R.drawable.textview_border_red);
+        }else if ("1".equals(status)) { //   执行中
+            holder.iv_ygd_status.setText("执行中");
+            holder.iv_ygd_status.setTextColor(Color.parseColor("#fdb96d"));
+            holder.iv_ygd_status.setBackgroundResource(R.drawable.textview_border_orange);
+        }else if ("2".equals(status)) { //   已归档
+            holder.iv_ygd_status.setText("已归档");
+            holder.iv_ygd_status.setTextColor(Color.parseColor("#f0f0f0"));
+            holder.iv_ygd_status.setBackgroundResource(R.drawable.textview_border_dark);
+        }
 //        holder.tv_piaohao.setText("");
 //        holder.tv_content.setText("");
 //        holder.tv_caozuoren.setText("");
--- app/src/main/java/tickets/nari/com/hubeitickets/fragment/ArchivedFragment.java
+++ app/src/main/java/tickets/nari/com/hubeitickets/fragment/ArchivedFragment.java
@@ -30,17 +30,13 @@ public class ArchivedFragment extends android.support.v4.app.Fragment {
      * 操作票列表信息集合
      */
     private List<ArchivedTicketListBean> ticketList = new ArrayList<ArchivedTicketListBean>();
-    private StringBuilder sb;
-    private String mUserName;
     private Integer page = 1;
-    private String mObjId;
-    private String mMbId;
     private CaoZuoPiao_List_Adapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO Auto-generated method stub
-        View v = inflater.inflate(R.layout.fragment_archiving, container, false);
+        View v = inflater.inflate(R.layout.fragment_caozuopiao, container, false);
         mPf_fragment_archived = (PullToRefreshLayout) v.findViewById(R.id.pf_fragment_archived);
         mLv_fragment_archived = (ListView) v.findViewById(R.id.lv_fragment_archived);
 
@@ -101,12 +97,11 @@ public class ArchivedFragment extends android.support.v4.app.Fragment {
         ticketList.add(archivedTicketListBean);
         if (mAdapter == null) {
 
-            mAdapter = new CaoZuoPiao_List_Adapter(getActivity(), ticketList);
+            mAdapter = new CaoZuoPiao_List_Adapter(getActivity(), ticketList,"2");
             mLv_fragment_archived.setAdapter(mAdapter);
 
         } else {
             mAdapter.notifyDataSetChanged();
-            //mLv_fragment_archived.setAdapter(mAdapter);
         }
     }
 
