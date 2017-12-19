90d555e5835bd6d015da7ebff9c7e4f019fbc43d
xieshibao <847019290@qq.com>
2017-12-19 11:59:40 +0800

    谢仕宝--添加

--- app/src/main/java/tickets/nari/com/hubeitickets/fragment/ArchivedFragment.java
+++ app/src/main/java/tickets/nari/com/hubeitickets/fragment/ArchivedFragment.java
@@ -1,26 +1,19 @@
 package tickets.nari.com.hubeitickets.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.nari.tickets.baselibrary.base.BasePullToRefresh;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import tickets.nari.com.hubeitickets.R;
-import tickets.nari.com.hubeitickets.activity.Ticket_MainActivity;
-import tickets.nari.com.hubeitickets.adapter.ArchivedAdapter;
+import tickets.nari.com.hubeitickets.adapter.CaoZuoPiao_List_Adapter;
 import tickets.nari.com.hubeitickets.bean.ArchivedTicketListBean;
 import tickets.nari.com.hubeitickets.customview.PullToRefreshLayout;
 
@@ -42,7 +35,7 @@ public class ArchivedFragment extends android.support.v4.app.Fragment {
     private Integer page = 1;
     private String mObjId;
     private String mMbId;
-    private ArchivedAdapter mAdapter;
+    private CaoZuoPiao_List_Adapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -108,7 +101,7 @@ public class ArchivedFragment extends android.support.v4.app.Fragment {
         ticketList.add(archivedTicketListBean);
         if (mAdapter == null) {
 
-            mAdapter = new ArchivedAdapter(getActivity(), ticketList);
+            mAdapter = new CaoZuoPiao_List_Adapter(getActivity(), ticketList);
             mLv_fragment_archived.setAdapter(mAdapter);
 
         } else {
